266376cb7488f45130b8d78726dff076980b20ae
srinivassvarks <srinivas.svarks@gmail.com>
2018-06-19 15:46:02 +0530

    19-6-18 seqlize

--- excelsample.js
+++ excelsample.js
@@ -2,22 +2,9 @@ var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var csv = require('csv-express');
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'sa',
-    database: 'test3'
-});
 var app = express();
-app.use(bodyParser.json());
-connection.connect(function (err) {
-    if (!err) {
-        console.log("Database is connected ... \n\n");
-    } else {
-        console.log("Error connecting database ... \n\n");
-    }  
-});
+
+
 
 app.get('/excel',function(req,res){
     
@@ -30,7 +17,7 @@ app.get('/excel',function(req,res){
         {"name": "Mary", "age": 2}
     ],false)
     });
-
+    
 
 app.listen(3000);
 console.log('Listening on port 3000');
